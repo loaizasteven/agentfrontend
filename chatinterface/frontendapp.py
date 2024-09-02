5f4908ec28dfd29ab8f526f4ba8c5f773ae59899
Steven Loaiza <steven.loaiza@outlook.com>
2024-09-02 09:32:04 -0400

    Programtic buttons

--- chatinterface/frontendapp.py
+++ chatinterface/frontendapp.py
@@ -2,21 +2,31 @@ import streamlit as st
 
 import os.path as osp
 import sys
+import uuid
 
-import pydantic
+from pydantic import BaseModel
 
 script_dir = osp.dirname(__file__)
 sys.path.insert(0, osp.dirname(script_dir))
 from _ui.generaloptions import change_button_style, detect_browser
 
+
 class ChatBotApp():
     title:str = 'My First LLM Chat'
+    buttons: tuple = (
+        ('Button 1', 'This is my short instruction content, with multiline instruction'),
+        ('Button 2', 'This is my short instruction content, with multiline instruction'),
+        ('Button 3', 'This is my short instruction content, with multiline instruction'),
+    )
 
     def _render(self):
         "Renders the chatbot UI"
         st.title(self.title)
-        st.button('Start Chat', key='test123')
-        change_button_style('Start Chat', 'Start Chat', 'Let me add more info to my content')
+
+        cols = st.columns(len(self.buttons))
+        for i, entry in enumerate(self.buttons):
+            cols[i].button(entry[0], key=uuid.uuid4())
+            change_button_style(entry[0], entry[0], entry[1])
 
         prompt = st.chat_input("Say something")
         if prompt:
@@ -26,7 +36,7 @@ class ChatBotApp():
                 st.write("Hello 👋, I am the Assistant")
 
     @staticmethod
-    def _check():
+    def _zcheck():
         "Static Method to check browser"
         with st.container():
             detect_browser()
@@ -34,7 +44,7 @@ class ChatBotApp():
     def __call__(self, debug:str = False):
         if debug:
             self._render
-            self._check
+            self._zcheck
         else:
             # Runs methods alphabetically, not ideal
             for method in dir(self): 
@@ -45,5 +55,5 @@ class ChatBotApp():
 
 if __name__=="__main__":
     ui = ChatBotApp()
-    ui(True)
+    ui(False)
     
